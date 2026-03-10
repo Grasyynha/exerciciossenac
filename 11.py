f76fbef48f9ad522d773acb7ba086140f82ed10b
Grasiela Texeira <grasy.mst@gmail.com>
2026-03-10 10:33:50 -0300

    Finalização dos exercicios

--- 11.py
+++ 11.py
@@ -1,2 +1,11 @@
 # Solicite ao usuário uma distância em metros e depois converta para km inteiros com //= 1000, 
-# guarde os metros restantes utilizando %= (utilize outra variável).  
+# guarde os metros restantes utilizando %= (utilize outra variável).
+metros = int(input("Digite a distancia em metros: "))  
+distancia = metros
+distancia //= 1000
+
+metros_restantes = metros
+metros_restantes %= 1000
+
+print("Quilmetros", metros)
+print("Metros restantes", metros_restantes)
