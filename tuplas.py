89bd6ce062a23a389ff0a3c2367d3b35d01b331e
Grasyynha <grasy.mst@gmail.com>
2026-03-16 20:49:57 -0300

    Adicionado mais um exercicio de tuplas

--- tuplas.py
+++ tuplas.py
@@ -2,4 +2,17 @@ dias = ("segunda", "terça", "quarta", "quinta", "sexta", "sabado", "domingo")
 print(dias)
 print(dias[0])
 print(dias[-1])
+
+# Tamanho
 print(len(dias))
+
+# Fatiamento em tuplas
+print("Primeiros 3 dias", dias[:3])
+
+# Verificar se um elemento em tuplas
+print("Tem 'segunda'?", "segunda" in dias)
+
+# Contagem e indice em tuplas
+print("Contagem de 'terça':", dias.count("terça"))
+print("Contagem de indice de 'quarta':", dias.index("quarta"))
+
