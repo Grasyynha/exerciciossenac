406cf7f95dadc02cc2e0e56acf8922792a8c02bc
Grasyynha <grasy.mst@gmail.com>
2026-03-16 20:27:42 -0300

    Adicionado mais um exercicio de listas

---
+++ listas2.py
@@ -0,0 +1,13 @@
+listaCompras = []
+
+# Adicionando elementos
+listaCompras.append("arroz")
+listaCompras.append("feijao")
+listaCompras.append("trigo")
+listaCompras.append("macarrao")
+listaCompras.append("azeite")
+listaCompras.append("vinagre")
+print(listaCompras)
+
+listaCompras.remove("arroz")
+print(listaCompras)
