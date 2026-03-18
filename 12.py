099f5c235135cbdaf81d4aa410ae8d133b4efcbe
Grasiela Texeira <grasY.mst@gmail.com>
2026-03-18 20:54:08 -0300

    tuplas

--- 12.py
+++
@@ -1,9 +0,0 @@
-lista_compras = []
-lista_compras.append("arroz")
-lista_compras.append("feijao")
-lista_compras.append("batata")
-lista_compras.append("farinha")
-print(lista_compras)
-
-lista_compras.remove("arroz")
-print(lista_compras)
