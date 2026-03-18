de8a21ebabf209d1444ff91e440fd2bd487f32ee
Grasiela Texeira <grasY.mst@gmail.com>
2026-03-18 20:53:58 -0300

    listas

---
+++ 12.py
@@ -0,0 +1,9 @@
+lista_compras = []
+lista_compras.append("arroz")
+lista_compras.append("feijao")
+lista_compras.append("batata")
+lista_compras.append("farinha")
+print(lista_compras)
+
+lista_compras.remove("arroz")
+print(lista_compras)
