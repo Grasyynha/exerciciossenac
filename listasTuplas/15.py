ca39ddc15de55b9e6cc6ef78257af8e47cf11754
Grasiela Texeira <grasy.mst@gmail.com>
2026-03-19 16:28:59 -0300

    Mais um exercicio adicionado

---
+++ listasTuplas/15.py
@@ -0,0 +1,9 @@
+#  [TUPLE] Dias da semana com tuplas
+
+dias = ("segunda", "terça", "quarta", "quinta", "sexta", "sabado", "domingo")
+print(dias)
+print(dias[0])
+print(dias[-1])
+
+# Tamanho
+print(len(dias))
---
+++ listasTuplas/16.py
@@ -0,0 +1,3 @@
+#[TUPLE - desafio] Tupla de notas com média (sem alterar a tupla)
+#Tarefa: Leia três notas (float) e armazene em uma tupla. Exiba a tupla e a média das notas.
+# Use: float(), sum(), len(), print()
