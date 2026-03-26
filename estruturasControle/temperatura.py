1f35a3132722c118a95b0b24b5b25aaa3d66019b
Grasiela Texeira <grasY.mst@gmail.com>
2026-03-25 21:23:03 -0300

    Temperatura

---
+++ estruturasControle/temperatura.py
@@ -0,0 +1,15 @@
+
+temp  = int(input("Digite a  temperatura atual: "))
+
+
+if temp < 10:
+    temperatura = "Muito frio"
+elif temp <= 10:
+    temperatura = "Agradável"
+elif temp < 25:
+    temperatura = "Quente"
+else:
+    temperatura = "Muito quente"            
+
+
+print(f"A temperatura está: - {temperatura}")            
