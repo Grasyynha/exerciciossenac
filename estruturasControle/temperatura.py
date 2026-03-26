2e31843bb514e8f0c7e8ba2ba96aba2d37f7aa90
Grasiela Texeira <grasY.mst@gmail.com>
2026-03-25 21:27:18 -0300

    Atualização de variável

--- estruturasControle/temperatura.py
+++ estruturasControle/temperatura.py
@@ -4,10 +4,13 @@ temp  = int(input("Digite a  temperatura atual: "))
 
 if temp < 10:
     temperatura = "Muito frio"
+
 elif temp <= 10:
     temperatura = "Agradável"
+
 elif temp < 25:
     temperatura = "Quente"
+    
 else:
     temperatura = "Muito quente"            
 
