4e518c1cccd3f2577f1f5a95b4268ca650eeed99
Grasiela Texeira <grasy.mst@gmail.com>
2026-03-30 15:57:50 -0300

    Conclusão dos exercicios

--- exercEstruturaControle/1.py
+++ exercEstruturaControle/1.py
@@ -1,2 +1,11 @@
 # Solicite ao usuário que informe um número e depois exiba
 #  se o número é positivo, negativo ou zero.
+
+numero = int(input("Digite um numero: "))
+
+if numero < 0:
+    print("O numero é negativo")
+elif numero > 0 :
+    print("O numero é positivo")
+else:
+    print("O numero é zero")        
